d84066007ec0661fb0056698997aeea302eaed55
Yaroslav <dzisyuk.yari@gmail.com>
2022-01-29 23:55:46 +0200

    Forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import modals from './modules/modals.js';
 import sliders from './modules/sliders.js';
+import forms from './modules/forms.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
     modals();
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    forms();
 });
