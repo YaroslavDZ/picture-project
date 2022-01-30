b5083ac40ea2fdcc356eeaaaef29fb5cee5bde64
Yaroslav <dzisyuk.yari@gmail.com>
2022-01-30 23:35:55 +0200

    Implemented mask in number inputs, only cyrillic symbols can be inputed in name and message inputs

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from './modules/modals.js';
 import sliders from './modules/sliders.js';
 import forms from './modules/forms.js';
+import mask from './modules/mask.js';
+import checkTextInputs from './modules/checkTextInputs.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -8,4 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"');
+    checkTextInputs('[name="message"');
 });
