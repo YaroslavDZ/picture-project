f2fa60fdc677fdf8bf1285f9c3914d9eba8f9166
Yaroslav <dzisyuk.yari@gmail.com>
2022-02-01 01:12:59 +0200

    Implemented the filtering of items

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ import mask from './modules/mask.js';
 import checkTextInputs from './modules/checkTextInputs.js';
 import showMoreStyles from './modules/showMoreStyles.js';
 import calc from './modules/calc.js';
+import filter from './modules/filter.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -17,4 +18,5 @@ window.addEventListener('DOMContentLoaded', () => {
     checkTextInputs('[name="message"');
     showMoreStyles('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
+    filter();
 });
