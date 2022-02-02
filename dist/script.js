603e190a81304b8c5bc01bcd1861f3b80f53a794
Yaroslav <dzisyuk.yari@gmail.com>
2022-02-02 20:59:34 +0200

    Drag&Drop

--- dist/script.js
+++ dist/script.js
@@ -4211,6 +4211,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_accordion_js__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./modules/accordion.js */ "./src/js/modules/accordion.js");
 /* harmony import */ var _modules_burger_js__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./modules/burger.js */ "./src/js/modules/burger.js");
 /* harmony import */ var _modules_scrolling_js__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./modules/scrolling.js */ "./src/js/modules/scrolling.js");
+/* harmony import */ var _modules_drop_js__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./modules/drop.js */ "./src/js/modules/drop.js");
+
 
 
 
@@ -4240,6 +4242,7 @@ window.addEventListener('DOMContentLoaded', function () {
   Object(_modules_accordion_js__WEBPACK_IMPORTED_MODULE_9__["default"])('.accordion-heading');
   Object(_modules_burger_js__WEBPACK_IMPORTED_MODULE_10__["default"])('.burger-menu', '.burger');
   Object(_modules_scrolling_js__WEBPACK_IMPORTED_MODULE_11__["default"])('.pageup');
+  Object(_modules_drop_js__WEBPACK_IMPORTED_MODULE_12__["default"])();
 });
 
 /***/ }),
@@ -4380,6 +4383,71 @@ var checkTextInputs = function checkTextInputs(selector) {
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (checkTextInputs);
+
+/***/ }),
+
+/***/ "./src/js/modules/drop.js":
+/*!********************************!*\
+  !*** ./src/js/modules/drop.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var core_js_modules_web_dom_collections_for_each__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! core-js/modules/web.dom-collections.for-each */ "./node_modules/core-js/modules/web.dom-collections.for-each.js");
+/* harmony import */ var core_js_modules_web_dom_collections_for_each__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_web_dom_collections_for_each__WEBPACK_IMPORTED_MODULE_0__);
+
+
+var drop = function drop() {
+  var fileInputs = document.querySelectorAll('[name="upload"]');
+  ['dragenter', 'dragleave', 'dragover', 'drop'].forEach(function (eventName) {
+    fileInputs.forEach(function (input) {
+      input.addEventListener(eventName, preventDefaults, false);
+    });
+  });
+  ['dragenter', 'dragover'].forEach(function (eventName) {
+    fileInputs.forEach(function (input) {
+      input.addEventListener(eventName, function () {
+        return highlight(input);
+      }, false);
+    });
+  });
+  ['dragleave', 'drop'].forEach(function (eventName) {
+    fileInputs.forEach(function (input) {
+      input.addEventListener(eventName, function () {
+        return unhighlight(input);
+      }, false);
+    });
+  });
+  fileInputs.forEach(function (input) {
+    input.addEventListener('drop', function (e) {
+      input.files = e.dataTransfer.files;
+    });
+  });
+
+  function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  function highlight(item) {
+    item.closest('.file_upload').style.border = '5px solid yellow';
+    item.closest('.file_upload').style.backgroundColor = 'rgba(0, 0, 0, .7)';
+  }
+
+  function unhighlight(item) {
+    item.closest('.file_upload').style.border = 'none';
+
+    if (item.closest('.calc_form')) {
+      item.closest('.file_upload').style.backgroundColor = '#fff';
+    } else {
+      item.closest('.file_upload').style.backgroundColor = '#ededed';
+    }
+  }
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (drop);
 
 /***/ }),
 
---
+++ src/js/modules/drop.js
@@ -0,0 +1,48 @@
+const drop = () => {
+    const fileInputs = document.querySelectorAll('[name="upload"]');
+
+    ['dragenter', 'dragleave', 'dragover', 'drop'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, preventDefaults, false);
+        });
+    });
+
+    ['dragenter','dragover'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => highlight(input), false);
+        });
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {   
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => unhighlight(input), false);
+        });
+    });
+
+    fileInputs.forEach(input => {
+        input.addEventListener('drop', (e) => {
+            input.files = e.dataTransfer.files;
+        });
+    });
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    function highlight(item) {
+        item.closest('.file_upload').style.border = '5px solid yellow';
+        item.closest('.file_upload').style.backgroundColor = 'rgba(0, 0, 0, .7)';
+    }
+
+    function unhighlight(item) {
+        item.closest('.file_upload').style.border = 'none';
+        if (item.closest('.calc_form')) {
+            item.closest('.file_upload').style.backgroundColor = '#fff';
+        } else {
+            item.closest('.file_upload').style.backgroundColor = '#ededed';
+        }
+    }
+};
+
+export default drop;
